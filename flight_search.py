71e1e9f2282d150707e49717f4ac61a5d136deaf
alisadaintanvir <alisadaintanvir@gmail.com>
2023-08-04 02:57:36 +0600

    updated all the files and added notification functionality using twillo

--- flight_search.py
+++ flight_search.py
@@ -1,9 +1,11 @@
 import requests
 import os
+from flight_data import FlightData
+
 
 class FlightSearch:
     def __init__(self):
-        self.flight_search_endpoint = "https://api.tequila.kiwi.com/locations/query?"
+        self.flight_search_endpoint = "https://api.tequila.kiwi.com"
         self.api_key = os.environ.get("FLIGHT_SEARCH-API")
 
     def get_destination_code(self, city):
@@ -14,6 +16,44 @@ class FlightSearch:
             "term": city
         }
 
-        response = requests.get(url=self.flight_search_endpoint, params=parameter, headers=header)
+        response = requests.get(url=f"{self.flight_search_endpoint}/locations/query?", params=parameter, headers=header)
         response.raise_for_status()
         return response.json()['locations'][0]["code"]
+
+    def check_flight(self, origin_city_code, destination_city_code, date_from, date_to):
+        header = {
+            "apikey": self.api_key,
+        }
+
+        parameter = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": date_from.strftime("%d/%m/%Y"),
+            "date_to": date_to.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "curr": "GBP",
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+        }
+
+        response = requests.get(url=f"{self.flight_search_endpoint}/v2/search?", params=parameter, headers=header)
+        response.raise_for_status()
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        return flight_data
--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 from dotenv import load_dotenv
 from data_manager import DataManager
-
+from datetime import datetime, timedelta
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
 from pprint import pprint
 
 load_dotenv()
 data_manager = DataManager()
 sheet_data = data_manager.get_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
 # pprint(sheet_data)
 
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
-
     for data in sheet_data:
         data["iataCode"] = flight_search.get_destination_code(data['city'])
     data_manager.destination_data = sheet_data
     data_manager.update_data()
 
+
+current_date = datetime.now() + timedelta(days=1)
+six_months_from_now = current_date + timedelta(days=6 * 30)
+
+ORIGIN_CITY_IATA = "LON"
+
+for destination in sheet_data:
+    flight = flight_search.check_flight(
+        ORIGIN_CITY_IATA,
+        destination['iataCode'],
+        current_date,
+        six_months_from_now
+    )
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} "
+                    f"to {flight.destination_city}-{flight.destination_airport}, "
+                    f"from {flight.out_date} to {flight.return_date}."
+        )
